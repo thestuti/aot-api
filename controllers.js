29cf428e045a9ae84161bcf29363da85d56136a3
Neeraj Lamsal <itskop520@gmail.com>
2022-10-01 07:29:48 +0545

    [refactored] controllers

--- controllers.js
+++ controllers.js
@@ -1,75 +1,80 @@
-// Importing the quotes
-const quotes = require("./data/quotes.js")
+const quotes = require("./data/quotes.js");
 
 const getAllQuotes = (req, res) => {
-	const count = parseInt(req.query.count)
+  const count = parseInt(req.query.count);
+  if (isNaN(count)) {
+    return res.status(400).json({
+      message: "count query must be an integer",
+    });
+  }
 
-	// If count is not specified, then send all quotes
-	if (!count)
-		return res.json({
-			success: true,
-			count: quotes.length,
-			quotes,
-		})
+  if (!count)
+    return res.json({
+      success: true,
+      count: quotes.length,
+      quotes,
+    });
 
-	if (count > quotes.length)
-		return res.status(400).json({
-			success: false,
-			error: `Specified number exceeds number of quotes. Total number of quotes right now are ${quotes.length}`,
-		})
+  if (count > quotes.length)
+    return res.status(400).json({
+      success: false,
+      error: `Specified number exceeds number of quotes. Total number of quotes right now are ${quotes.length}`,
+    });
 
-	if (count < 0)
-		return res.status(400).json({
-			success: false,
-			error: "Are you retarded?",
-		})
+  if (count < 0)
+    return res.status(400).json({
+      success: false,
+      error: "Are you retarded?",
+    });
 
-	// If count is specified and is above 0 and not more than no. of existing quotes
-	res.json({
-		success: true,
-		count,
-		quotes: quotes.slice(0, count),
-	})
-}
+  res.json({
+    success: true,
+    count,
+    quotes: quotes.slice(0, count),
+  });
+};
 
 const getRandomQuote = (req, res) => {
-	const count = parseInt(req.query.count)
+  const count = parseInt(req.query.count);
 
-	// If count is not specified, then send a random quote
-	if (!count)
-		return res.json({
-			success: true,
-			count: quotes.length,
-			quote: quotes[Math.floor(Math.random() * quotes.length)],
-		})
+  if (isNaN(count)) {
+    return res.status(400).json({
+      message: "count query must be an integer",
+    });
+  }
+  if (!count)
+    return res.json({
+      success: true,
+      count: quotes.length,
+      quote: quotes[Math.floor(Math.random() * quotes.length)],
+    });
 
-	if (count > quotes.length)
-		return res.status(400).json({
-			success: false,
-			error: `Specified number exceeds number of quotes. Total number of quotes right now are ${quotes.length}`,
-		})
+  if (count > quotes.length)
+    return res.status(400).json({
+      success: false,
+      error: `Specified number exceeds number of quotes. Total number of quotes right now are ${quotes.length}`,
+    });
 
-	if (count < 0)
-		return res.status(400).json({
-			success: false,
-			error: "Are you retarded?",
-		})
+  if (count < 0)
+    return res.status(400).json({
+      success: false,
+      error: "Are you retarded?",
+    });
 
-	//  If count is specified, then send 'N' random quotes
-	const data = []
+  const data = [];
 
-	for (let i = 0; i < count; i++) {
-		data.push(quotes[Math.ceil(Math.random() * quotes.length)])
-	}
+  for (let i = 0; i < count; i++) {
+    data.push(quotes[Math.ceil(Math.random() * quotes.length)]);
+  }
 
-	res.json({
-		success: true,
-		count,
-		quotes: data,
-	})
-}
+  res.json({
+    success: true,
+    count,
+    quotes: data,
+  });
+};
 
 module.exports = {
-	getAllQuotes,
-	getRandomQuote,
-}
+  getAllQuotes,
+  getRandomQuote,
+};
