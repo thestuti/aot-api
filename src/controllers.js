2f97914b693245f38cf78986b2585c08ecc4edaf
washbin <76929116+washbin@users.noreply.github.com>
2022-10-10 20:21:18 +0545

    chore: standardize both success and failure response structures

--- src/controllers.js
+++ src/controllers.js
@@ -3,60 +3,76 @@ import { quotes } from "../data/quotes.js";
 const getAllQuotes = (req, res) => {
   if (!req.query.count) {
     return res.json({
-      count: quotes.length,
-      quotes,
+      data: {
+        count: quotes.length,
+        quotes,
+      },
     });
   }
 
   const count = parseInt(req.query.count, 10);
   if (isNaN(count)) {
     return res.status(400).json({
-      error: "count query must be an integer",
+      error: {
+        message: "count query must be an integer",
+      },
     });
   }
 
   if (count > quotes.length) {
     return res.status(400).json({
-      error: `Specified number exceeds number of quotes. Total number of quotes right now are ${quotes.length}`,
+      error: {
+        message: `Specified number exceeds number of quotes. Total number of quotes right now are ${quotes.length}`,
+      },
     });
   }
 
   if (count < 0) {
     return res.status(400).json({
-      error: "Are you retarded?",
+      error: {
+        message: "Are you retarded?",
+      },
     });
   }
 
   return res.json({
-    count,
-    quotes: quotes.slice(0, count),
+    data: {
+      count,
+      quotes: quotes.slice(0, count),
+    },
   });
 };
 
 const getRandomQuote = (req, res) => {
   if (!req.query.count) {
     return res.json({
-      count: 1,
-      quotes: [quotes[Math.ceil(Math.random() * quotes.length)]],
+      data: {
+        count: 1,
+        quotes: [quotes[Math.ceil(Math.random() * quotes.length)]],
+      },
     });
   }
 
   const count = parseInt(req.query.count, 10);
   if (isNaN(count)) {
     return res.status(400).json({
-      error: "count query must be an integer",
+      error: { message: "count query must be an integer" },
     });
   }
 
   if (count > quotes.length) {
     return res.status(400).json({
-      error: `Specified number exceeds number of quotes. Total number of quotes right now are ${quotes.length}`,
+      error: {
+        message: `Specified number exceeds number of quotes. Total number of quotes right now are ${quotes.length}`,
+      },
     });
   }
 
   if (count < 0) {
     return res.status(400).json({
-      error: "Are you retarded?",
+      error: {
+        message: "Are you retarded?",
+      },
     });
   }
 
@@ -66,14 +82,18 @@ const getRandomQuote = (req, res) => {
   }
 
   return res.json({
-    count,
-    quotes: data,
+    data: {
+      count,
+      quotes: data,
+    },
   });
 };
 
 const notFoundHandler = (req, res) => {
   res.status(404).json({
-    error: `No such route: ${req.path}`,
+    error: {
+      message: `No such route: ${req.path}`,
+    },
   });
 };
 
